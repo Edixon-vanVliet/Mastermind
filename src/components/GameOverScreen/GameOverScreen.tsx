a03b15c2fc388521856579b673ee98a79be52d7b
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-11-27 16:16:19 -0400

    Use React.memo to avoid re-rendering

--- src/components/GameOverScreen/GameOverScreen.tsx
+++ src/components/GameOverScreen/GameOverScreen.tsx
@@ -1,26 +1,29 @@
+import React from "react";
 import showColors from "../../helpers/Colors/showColors";
 import "./GameOverScreen.css";
 
-const GameOverScreen = (props: { message: string; code: number[] }) => {
-	return (
-		<div id="GameOverScreen">
-			<div>
+const GameOverScreen = React.memo(
+	(props: { message: string; code: number[] }) => {
+		return (
+			<div id="GameOverScreen">
 				<div>
-					<h1>{props.message}</h1>
-					<h2>The code was</h2>
-					<div id="code">{showColors(props.code)}</div>
-				</div>
-				<div>
-					<button
-						className="rebeccapurple"
-						onClick={() => window.location.reload()}
-					>
-						Play again
-					</button>
+					<div>
+						<h1>{props.message}</h1>
+						<h2>The code was</h2>
+						<div id="code">{showColors(props.code)}</div>
+					</div>
+					<div>
+						<button
+							className="rebeccapurple"
+							onClick={() => window.location.reload()}
+						>
+							Play again
+						</button>
+					</div>
 				</div>
 			</div>
-		</div>
-	);
-};
+		);
+	}
+);
 
 export default GameOverScreen;
