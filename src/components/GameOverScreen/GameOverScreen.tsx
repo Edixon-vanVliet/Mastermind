901fb6c7d85c26fe232e6760fa2aae517f2cfd4c
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-11-27 16:18:12 -0400

    Create an interface for GameOverScreen props

--- src/components/GameOverScreen/GameOverScreen.tsx
+++ src/components/GameOverScreen/GameOverScreen.tsx
@@ -1,29 +1,33 @@
 import React from "react";
+import Colors from "../../helpers/Colors/Colors";
 import showColors from "../../helpers/Colors/showColors";
 import "./GameOverScreen.css";
 
-const GameOverScreen = React.memo(
-	(props: { message: string; code: number[] }) => {
-		return (
-			<div id="GameOverScreen">
+export interface IGameOverScreenProps {
+	message: string;
+	code: Colors[];
+}
+
+const GameOverScreen = React.memo(({ message, code }: IGameOverScreenProps) => {
+	return (
+		<div id="GameOverScreen">
+			<div>
 				<div>
-					<div>
-						<h1>{props.message}</h1>
-						<h2>The code was</h2>
-						<div id="code">{showColors(props.code)}</div>
-					</div>
-					<div>
-						<button
-							className="rebeccapurple"
-							onClick={() => window.location.reload()}
-						>
-							Play again
-						</button>
-					</div>
+					<h1>{message}</h1>
+					<h2>The code was</h2>
+					<div id="code">{showColors(code)}</div>
+				</div>
+				<div>
+					<button
+						className="rebeccapurple"
+						onClick={() => window.location.reload()}
+					>
+						Play again
+					</button>
 				</div>
 			</div>
-		);
-	}
-);
+		</div>
+	);
+});
 
 export default GameOverScreen;
