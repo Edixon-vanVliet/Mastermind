561ba49ba99206301d6166acc0788dd9099fced5
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2020-11-27 19:07:25 -0400

    Fix bug where color could be greater than 6

--- src/helpers/Colors/generateRandomColor.tsx
+++ src/helpers/Colors/generateRandomColor.tsx
@@ -4,8 +4,9 @@ const generateRandomColor = () => {
 	let color = 0;
 
 	do {
+		debugger;
 		color = Math.round(Math.random() * (Object.keys(Colors).length / 2));
-	} while (color === 0);
+	} while (color === 0 || color > 6);
 
 	return color;
 };
